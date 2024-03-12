3b9e5d1958dccacd0df6fba92b43e7883edd530e
Kirill <kirill.nikolaev@everon.fi>
2024-03-12 10:21:47 +0200

    create class rodentia

---
+++ rodentia.py
@@ -0,0 +1,18 @@
+from mammals import Mammals
+
+
+class Rodentia(Mammals):
+    def __init__(self, mammary_glands, hair_fur, warm_bloodedness, viviparity, middle_ear_bones, sexual_dimorphism, continuously_growing_incissors, high_reproduction_rate, representative):
+        super().__init__(mammary_glands, hair_fur, warm_bloodedness, viviparity, middle_ear_bones, sexual_dimorphism)
+        self.continuously_growing_incissors = continuously_growing_incissors
+        self.high_reproduction_rate = high_reproduction_rate
+        self.representative = "Mouse" | "Squirrel" | "Beaver"
+
+    def gnaw(self):
+        pass
+
+    def burrow(self):
+        pass
+
+    def hoardFood(self):
+        pass
