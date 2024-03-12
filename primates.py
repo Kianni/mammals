ad7a89e3777bf14a4a17c9ce2ed02ab09561508b
Kirill <kirill.nikolaev@everon.fi>
2024-03-12 10:32:19 +0200

    create class Primates

---
+++ primates.py
@@ -0,0 +1,19 @@
+from mammals import Mammals
+
+
+class Primates(Mammals):
+    def __init__(self, mammary_glands, hair_fur, warm_bloodedness, viviparity, middle_ear_bones, sexual_dimorphism, developed_brain, social_behaviour, opposite_thumb, representative):
+        super().__init__(mammary_glands, hair_fur, warm_bloodedness, viviparity, middle_ear_bones, sexual_dimorphism)
+        self.developed_brain = developed_brain
+        self.social_behaviour = social_behaviour
+        self.opposite_thumb = opposite_thumb
+        self.representative = 'Monkey'
+
+    def socialize(self):
+        pass
+
+    def toolUse(self):
+        pass
+
+    def arborealMovement(self):
+        pass
